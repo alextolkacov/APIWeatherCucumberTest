16c28f1afce7c6ba2b0f61af4f27b5da5f212fdc
Alex <aleksandr.tolkacov@gmail.com>
2018-08-15 23:03:04 +0300

    Update

--- src/test/java/testDefinitions/WeatherRequester.java
+++ src/test/java/testDefinitions/WeatherRequester.java
@@ -3,6 +3,7 @@ package testDefinitions;
 import model.Response;
 import org.springframework.web.client.RestTemplate;
 
+
 public class WeatherRequester {
     public final String URL = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22";
 
